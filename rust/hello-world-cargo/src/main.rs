efbfa5907acc3b0dd1da0acbdcaad7a5a750f685
shravankumar9892 <shravankumarshetty9892@gmail.com>
2020-04-17 00:45:03 +0530

    if/else added

--- rust/hello-world-cargo/src/main.rs
+++ rust/hello-world-cargo/src/main.rs
@@ -1,7 +1,28 @@
 fn main() {
-    // This is an example of Hello World
     /* Author: Shravankumar Shetty
        Organization: Dotmole
     */
-    println!("Hello, world!");
+
+    // Print Vishala
+    let mut x = "Vishala";
+    println!("Hello, {}!", x);
+    
+    // Print Shravan
+    x = "Shravan";
+    println!("Hello, {}!", x);
+
+    // Using variable types
+    let _y: u64 = 45;
+    let _f: f32 = 6.7;
+    let _b: bool = false;
+
+    let z = 45;
+
+    if (z == 45) {
+        println!("45 found!");
+    } else if z < 45 {
+        println!("Less than 45!");
+    } else {
+        println!("More than 45");
+    }
 }
